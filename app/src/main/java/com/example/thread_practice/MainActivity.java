57109898a8bb800062ca856555f5a641ae1509a5
gn000123 <gn000123@naver.com>
2021-06-11 10:39:35 +0900

    qwe

--- app/src/main/java/com/example/thread_practice/MainActivity.java
+++ app/src/main/java/com/example/thread_practice/MainActivity.java
@@ -2,21 +2,35 @@ package com.example.thread_practice;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private Button button1, button2, button3;
-    private Context context;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    public static RecyclerView recyclerView;
     private Handler handler;
-    private TextView textView;
+    public static MyAdapter adapter;
+    public static Context context;
+    public static ArrayList<Post> myList = new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
     // 메인 스레드는 원래 루퍼 있음. 어지간하면 루퍼 신경 안 써도 될듯
     // 루퍼 = 메세지 큐에서 메세지 꺼내서 핸들러한테 줌
     // 헨들러는 메인스레드에 종속되어 있음.
@@ -27,84 +41,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.context =context;
         initView();
+        this.context = context;
 
-//        Thread thread = new Thread(new Runnable() { // 2021-06-11 todo
-//            @Override
-//            public void run() {
-//                // JSON 파싱해서
-//                handler.post(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        // 띄워주기
-//                    }
-//                });
-//            }
-//        });
-//        textView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//            thread.start();
-//            }
-//        });
 
-        Thread thread1 = new Thread(new Runnable() {
+
+        Thread thread = new Thread(new Runnable() { // 2021-06-11 todo
             @Override
             public void run() {
+
+
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
-                        textView.setText("1");
+
+                        MyResponse myResponse = new MyResponse();
+                        myResponse.getPostResponse();
+
                     }
                 });
             }
         });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        textView.setText("2");
-                    }
-                });
-            }
-        });
-        Thread thread3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        textView.setText("3");
-                    }
-                });
-            }
-        });
-        button1.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                thread1.start();
+            thread.start();
             }
         });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                thread2.start();
-            }
-        });
-        button3.setOnClickListener(v -> {
-            thread3.start();
-        });
-// 재사용하면 앱 뻗음
+
+
+
+
     }
     public void initView() {
         handler = new Handler();
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new MyAdapter(myList, MainActivity.this);
+        linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+
     }
 }
 
